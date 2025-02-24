010c3149ba695f92ad94aca6fbd22c2d50c8b2df
Mishych <mtcebak@gmail.com>
2025-02-24 15:47:51 +0200

    Added new file with secrets

---
+++ test3.py
@@ -0,0 +1,55 @@
+import os
+import re
+import json
+from colored import fg, attr
+import logging
+import aiohttp
+import asyncio
+import aiofiles
+import argparse
+
+API_KEY="sk_98JHGT9NH98H0p2yX98HKJH0"
+AUTH_TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0"
+
+
+real_secret_key = "sk_test_51Hn2W6D5xxEXAMPLE5sX9cJpt"
+
+oauth_token = "1a79fjoqowi74hfkvd8375kbmpd93"
+
+jwt_secret = "supersecurejwtsecret"
+auth_token = "0a1b2c3d4e5f6g7h8i9j0k"
+
+database_table_name = "users"
+logging.info("User logged in successfully")
+error_message = "NullPointerException occurred"
+debug_message = "This is a debug message"
+sample_variable = "Hello, world!"
+
+parol="1939njfjslxnbn2"
+AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMPLE
+AWS_SECRET_ACCESS_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
+passs = "jifjoisjef0909023rr"
+
+"-----BEGIN DSA PRIVATE KEY-----"
+
+fake_api_key = "1234567890ABCDEF"  # Маскований ключ, який не є справжнім
+
+test_token = "ghp_XXXXXXXXXXXXXXXXXXXXXXXXX"  # Токен, який містить правильний префікс, але не є дійсним
+
+sample_access_key = "AKIAZZZZZZZZZZZZZZZZ"  # AWS ключ, який явно є підробленим
+
+config_secret = "not_a_real_secret_but_looks_like_one"  # Секретоподібний рядок
+
+placeholder_password = "password123"  # Типове значення для паролів
+
+database_connection = "mysql://user:password@localhost/db"  # Локальне підключення, не є справжнім
+
+example_token = "ghs_example1234567890"  # Формат схожий на GitHub token, але не дійсний
+
+mock_secret = "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz"  # Шаблонне значення
+
+test_private_key = "-----BEGIN PRIVATE KEY----- ABCDEFGHIJKLMNOPQRSTUVWXYZ -----END PRIVATE KEY-----"  # Фейковий ключ
+
+random_auth_key = "1234-5678-9101-1121"  # Не є справжнім, але виглядає як ключ доступу
+
+aws_key = "AKIATEST1234567890"  # Тестовий AWS ключ
