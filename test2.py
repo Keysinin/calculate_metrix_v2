76b1aead0585af2e4f8f8564398c623a2ce0f330
Mishych <mtcebak@gmail.com>
2025-02-24 15:08:55 +0200

    Added new file with secrets

---
+++ test2.py
@@ -0,0 +1,26 @@
+api_key = "AIzaSyD-3WlL8Vw8d5sHqjJ3h3Xy0FVW"
+token = "ghp_ABCDEFGH1234567890ijklMNOPQRSTUVWXYZ"
+access_key = "AKIAIOSFODNN7EXAMPLE"
+aws_secret_access_key = "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"
+db_password = "SuperSecret123!"
+slack_webhook_url = "https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX"
+
+private_key = "-----BEGIN PRIVATE KEY----- MIIBVwIBADANBgkqhkiG9w0BAQEFAASCAT8wggE7AgEAAkEAzB0FZk7n5qvTcEZy -----END PRIVATE KEY-----"
+
+test_api_key = "1234567890ABCDEF"
+fake_token = "ghp_XXXXXXXXXXXXXXXXXXXXXXXXX"
+random_string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+url = "https://example.com"
+config_setting = "enabled"
+
+test_data = {
+                            'default': 
+                                {
+                                    'aws_access_key_id': 'AKIA837F92D2K9UF7U2N',
+                                    'aws_secret_access_key': '9IJ7D73638HFU76X0KV8JKS21KIGHCYJEI98WNOG'
+                                },
+                            'test1': 
+                                {
+                                    'aws_access_key_id': 'AKIAGNGF92D2K9UF7NOG',
+                                    'aws_secret_access_key': 'NNN7D73638HFU76X0KV8JKS21KIGHCYJEI98WGGG'}
+                            }
